0e7e3cf68a687b5dcd41b761855cba21d3a43f06
Martin <rauch83@yandex.ru>
2022-09-01 02:28:43 +0300

    wq# Changes to be committed:

--- up/Program.cs
+++ up/Program.cs
@@ -3,32 +3,33 @@
                  {8,4,2,4},};
 int row = 0;
 int column = 0;
-int step = 1;
+int count = 0;
 int temp = 0;
 while(row < number.GetLength(0))
-{
-    while(column < number.GetLength(0))
+{   int step = 1;
+    while(count < number.GetLength(0)+1)
     {
-
+        
         if ((number[row, column]) < (number[row,(column + step)]))
         {
             temp = number[row, column];
             number[row, column] = number[row, column + step];
             number[row, column + step] = temp;
+            for(int i = 0; i < number.GetLength(0); i++)
+                {
+                    for(int j = 0; j < number.GetLength(0)+1; j++)
+                    {
+                        Console.Write(number[i,j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            Console.WriteLine(" ");
             step++;
 
-        }
-        step++;
+        }        
+        count++;
     }
     row++;
-
-    for(int i = 0; i < number.GetLength(0); i++)
-        {
-            for(int j = 0; j < number.GetLength(0); j++)
-            {
-                Console.Write(number[i,j] + " ");
-            }
-            Console.WriteLine();
-        }
     }
+
 
